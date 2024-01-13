a5195eec16c965085ba9e8510778355a4ef67bb6
ida <discoverywant30@gmail.com>
2024-01-13 17:36:32 +0900

    7segまで動作確認 matのピン番号設定

--- mineSweeper.ino
+++ mineSweeper.ino
@@ -1,13 +1,21 @@
 #include "ae7seg.h"
-uint8_t SPEAKER   =  3;
+uint8_t SPEAKER   =  2;
 uint8_t VOLUME    = A2;
 uint8_t BUTTON_A  = A3;
 uint8_t BUTTON_B  = A4;
 uint8_t BUTTON_C  = A5;
 uint8_t SEG_LATCH = A1;
-uint8_t SEG_SDI   =  3
+uint8_t SEG_SDI   =  3;
 uint8_t SEG_SCK   =  4;
-int jikkennyou = 0;
+uint8_t REG_SER   = 11;
+uint8_t REG_LATCH = 12;
+uint8_t REG_CLK   = 14;
+uint8_t MAT_7     = 10;
+uint8_t MAT_8     =  9;
+uint8_t MAT_9     =  8;
+uint8_t MAT_10    =  7;
+uint8_t MAT_11    =  6;
+uint8_t MAT_12    =  5;
 
 void setup() {
   Serial.begin(9600);
@@ -19,6 +27,9 @@ void setup() {
   pinMode(SEG_LATCH, OUTPUT);
   pinMode(SEG_SDI, OUTPUT);
   pinMode(SEG_SCK, OUTPUT);
+  pinMode(REG_SER, OUTPUT);
+  pinMode(REG_LATCH, OUTPUT);
+  pinMode(REG_, OUTPUT);
 }
 
 void loop() {
