11991cf9b8fd09437574adf5e1e03682c5651294
ida <discoverywant30@gmail.com>
2024-01-13 10:38:46 +0900

    パーツごとにファイル分け

---
+++ button.ino
@@ -0,0 +1,45 @@
+bool buttonFlag = false;
+void buttonA () {
+  if (buttonIsON(BUTTON_A)) {
+    switch (getMode()) {
+      case 1://上
+      break;
+      case 2://下
+      break;
+      case 3://左
+      break;
+      case 4://右
+      break;
+    }
+  }
+}
+
+
+void buttonB () {
+  if (buttonIsON(BUTTON_B)) {
+
+  }
+}
+
+void buttonC () {
+  if (buttonIsON(BUTTON_C)) {
+
+  }
+}
+
+
+bool buttonIsON (uint8_t button) {
+  delay(50);
+  if (digitalRead(button)==LOW && !buttonFlag) {
+    buttonFlag = true;
+    sound(button);
+    return 1;
+  }
+  else if (digitalRead(button)==LOW && buttonFlag) {
+    return 0;
+  }
+  else {
+    buttonFlag = false;
+    return 0;
+  }
+}
---
+++ sound.ino
@@ -0,0 +1,26 @@
+const int doremi[] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047};
+
+void sound(uint8_t part) {
+  if (part==BUTTON_A) {
+    tone(SPEAKER, doremi[5]);
+    delay(20);
+  }
+  else if (part==BUTTON_B) {
+    tone(SPEAKER, doremi[20]);
+    delay(35);
+    tone(SPEAKER, doremi[18]);
+    delay(15);
+  }
+  else if (part==BUTTON_C) {
+    tone(SPEAKER, doremi[18]);
+    delay(35);
+    tone(SPEAKER, doremi[20]);
+    delay(35);
+    tone(SPEAKER, doremi[22]);
+    delay(35);
+  }
+  else if (part==VOLUME) {
+    tone(SPEAKER, doremi[2]);
+    delay(10);
+  }
+}
